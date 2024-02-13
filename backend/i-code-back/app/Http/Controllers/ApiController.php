bf0628fcddcde2d88c804263259f166c9e622444
Efaz <ishrakul236@gmail.com>
2024-02-14 01:47:23 +0600

    i-code-multi-vendor

---
+++ backend/i-code-back/app/Http/Controllers/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getUsers()
+    {
+        $getUsers = User::get();
+        return $getUsers;
+    }
+}
